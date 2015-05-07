42c8f834c94324d7846f1f9a41c4587fd85855f9
David Hearn <davidhearn90@gmail.com>
2015-05-07 08:58:43 -0700

    finished milestone 2

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+//configuration for our php server
+set_time_limit(0);
+int_set('default_socket_timeout', 300);
+session_start();
+
+
+//make constants using define
+define('client_id', '8c8b323d7d494c0ba038030f9b40d7a4');
+define('client_secret', 'fde357320b494c5793ada5433a32c741');
+define('redirectURI', 'http://localhost:8888/appacademyapi/index.php');
+define('client_id', '8c8b323d7d494c0ba038030f9b40d7a4');
+
+?>
+
+
+
+CLIENT ID	8c8b323d7d494c0ba038030f9b40d7a4
+CLIENT SECRET	fde357320b494c5793ada5433a32c741
+WEBSITE URL	http://localhost:8888/appacademyapi/index.php
+REDIRECT URI	http://localhost:8888/appacademyapi/index.php
