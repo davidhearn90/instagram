5dface626d3f43c1789a8d82ed5b60e214fa3b86
David Hearn <davidhearn90@gmail.com>
2015-05-08 09:00:08 -0700

    finished milestone 3

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php 
 //configuration for our php server
 set_time_limit(0);
-int_set('default_socket_timeout', 300);
+ini_set("default_socket_timeout", 300);
 session_start();
 
 
 //make constants using define
-define('client_id', '8c8b323d7d494c0ba038030f9b40d7a4');
-define('client_secret', 'fde357320b494c5793ada5433a32c741');
-define('redirectURI', 'http://localhost:8888/appacademyapi/index.php');
-define('client_id', '8c8b323d7d494c0ba038030f9b40d7a4');
+define('client_ID', '8c8b323d7d494c0ba038030f9b40d7a4');
+define('client_ecret', 'fde357320b494c5793ada5433a32c741');
+define("redirectURI", "http://localhost/appacademyapi/index.php");
+define('ImageDirectory', 'pics/');
 
 ?>
 
-
-
-CLIENT ID	8c8b323d7d494c0ba038030f9b40d7a4
-CLIENT SECRET	fde357320b494c5793ada5433a32c741
-WEBSITE URL	http://localhost:8888/appacademyapi/index.php
-REDIRECT URI	http://localhost:8888/appacademyapi/index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="description" content="">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<title>Instagram Project</title>
+ 	<link rel="stylesheet" type="text/css" href="css/style.css">
+ 	<link rel="author" href="humans.txt">
+ </head>
+</head>
+<body>
+ <!-- Creating a login for people to go and give approval for ourr web app to access their Instagram Account 
+ After getting approval we are now going to have the information so that we can play with it.
+ --><a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>& redirect_uri=<?php echo redirectURI ?>&response_type=code">Login</a>
+ <script src="js/main.js"></script>
+</body>
+</html>
